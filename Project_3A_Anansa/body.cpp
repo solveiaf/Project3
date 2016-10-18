06769da7f6f190b3b27fb02ed07a70e82cdbf805
solveiaf <solveiaf@student.matnat.uio.no>
2016-10-18 16:04:35 +0200

    Added verlet algortigm

--- Project_3A_Anansa/body.cpp
+++ Project_3A_Anansa/body.cpp
@@ -18,3 +18,14 @@ double Body::getRadii() {
 void Body::calculateVelocity(double r, double mass1, double mass2) {
 
 }
+
+void calculateVelocityVerlet(double mass2)  {
+    //Algortihm for calculating velocity verlet
+    for (int i = 0; i<n; i++) {
+        x[i+1] = x[i] + h*vx[i] + ((h*h)/2.) *a;
+        y[i+1] = y[i] + h*vy[i] + ((h*h)/2.) *a;
+        vx[i+1] = vx[i] + h*a;
+        vy[i+1] = vy[i] + h*a
+
+}
+}
