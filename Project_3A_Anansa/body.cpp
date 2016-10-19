2b5cdaf4360c997f55f31f2d86eced479c93542d
Sarah <sarah.rastad@gmail.com>
2016-10-19 14:24:58 +0200

    Fiksa litt mer

--- Project_3A_Anansa/body.cpp
+++ Project_3A_Anansa/body.cpp
@@ -19,7 +19,8 @@ double Body::getRadii(double x, double y) {
     return sqrt(pow(x,2) + pow(y,2));
 }
 
-void Body::calculateVelocityVerlet(double mass2) {
+void Body::calculateVelocityVerlet(double n, double year) {
+    double h = year/n;
         //Algortihm for calculating velocity verlet
     for (int i = 0; i<n; i++) {
         x[i+1] = x[i] + h*vx + ((h*h)/2.) *a;
@@ -28,16 +29,17 @@ void Body::calculateVelocityVerlet(double mass2) {
         vy += h*a;
 
     }
+    return;
 }
 
-void Body::calculateVelocityEuler(double n, double time) {
-    double dt = time/n;
+void Body::calculateVelocityEuler(double n, double year) {
+    double dt = year/n;
     for (int i=0; i<n; i++) {
         r = Body::getRadii(x[i],y[i]);
-        x[i+1] = x[i] + vx*dt;
-        y[i+1] = y[i] + vy*dt;
-        vx -= (dt*4*pi*pi*pow(AU,3))/(pow(r,3)*time)*x[i];
-        vy -= (dt*4*pi*pi*pow(AU,3))/(pow(r,3)*time)*y[i];
+        x[i+1] = x[i] + vx*h;
+        y[i+1] = y[i] + vy*h;
+        vx -= (h*4*pi*pi*pow(AU,3))/(pow(r,3)*year)*x[i];
+        vy -= (h*4*pi*pi*pow(AU,3))/(pow(r,3)*year)*y[i];
     }
     return;
 }
