50dca2dbbafdb40391cf0a0243aa3144cd0fff8a
solveiaf <solveiaf@student.matnat.uio.no>
2016-10-20 12:15:20 +0200

    Flyttet ting til system og skrev force og acceleration

--- Project_3A_Anansa/system.cpp
+++ Project_3A_Anansa/system.cpp
@@ -10,3 +10,31 @@ System::System()
 void System::readFile() {
     ifile = ifstream
 }
+
+
+double System::calculateForce(double x, double y,int number_particle,double m1, double m2){
+    double temp_r, temp, Fx, Fy;
+    double F = new double [1];
+    temp_r = getRadii(x,y);
+    if (number_particle == 2) {
+        temp = -(4*pi*pi*m1)/pow(temp_r,3);
+        Fx = temp*x;
+        Fy = temp*y;
+    }
+    else if (number_particle>2) {
+        temp = - (G*m1*m2)/pow(temp_r,3);
+        Fx = temp*x;
+        Fy = temp*y;
+
+    }
+    F = [Fx,Fy];
+    return * F;
+
+
+}
+
+
+double System::getRadii(double x, double y){
+    return sqrt(pow(x,2) + pow(y,2));
+}
+
